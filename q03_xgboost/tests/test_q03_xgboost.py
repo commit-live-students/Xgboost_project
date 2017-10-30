b46716331dfb74873b3a66444a4bb864af02ef14
Alex Anthony <alex19427@gmail.com>
2017-10-30 17:10:38 +0530

    Update test_q03_xgboost.py

--- q03_xgboost/tests/test_q03_xgboost.py
+++ q03_xgboost/tests/test_q03_xgboost.py
@@ -22,14 +22,16 @@ class TestXgboost(TestCase):
 
         # Return data types
 
-        accuracy = xgboost(X_train, X_test, y_train, colsample_bytree=0.7, gamma=0.9, max_depth=2, min_child_weight=6,
-                      subsample=1, n_estimators=100, reg_alpha=0, reg_lambda=0.05, learning_rate=0.1)
+        accuracy = xgboost(X_train, X_test, y_train,subsample=0.8,
+              colsample_bytree=0.7, max_depth=2, 
+              min_child_weight=4, reg_alpha=0, reg_lambda=1.0,
+             gamma=0,n_estimators=100,learning_rate=0.1)
 
         self.assertIsInstance(accuracy, float,
                               "Expected data type for return value is `list`, you are returning %s" % (
                                   type(accuracy)))
 
         # Return value tests
-        expected_accuracy = 0.802197802198
+        expected_accuracy = 0.79670329670329665
         self.assertAlmostEqual(accuracy, expected_accuracy, 3, "Expected accuracy does not match given accuracy")
 
