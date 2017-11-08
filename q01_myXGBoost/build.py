efa0ccd5a903dfb78b215419ce19b09c6e4f07d2
sudharsanasai <Sudharsanasai@gmail.com>
2017-11-08 02:35:50 +0000

    Done

--- q01_myXGBoost/build.py
+++ q01_myXGBoost/build.py
@@ -17,9 +17,14 @@ param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
                "colsample_bytree": [0.6, .7, .8, .9, 1]
                }
 
-
+model = XGBClassifier(seed=9)
 # Write your solution here :
 
+# Write your solution here :
+def myXGBoost(X_train,X_test,y_train,y_test,model,param_grid1,KFold=3,**kwargs):
 
-
-
+    model2 = GridSearchCV(model,param_grid1,cv=KFold)
+    model2.fit(X_train,y_train)
+    accuracy = accuracy_score(y_test,model2.predict(X_test))
+    best_params = model2.best_params_
+    return accuracy,best_params
