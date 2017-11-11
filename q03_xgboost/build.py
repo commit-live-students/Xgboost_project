bc3bd315410f27497e363a3f0068456a61d9de68
satishkhanna <snykhanna@gmail.com>
2017-11-11 10:22:39 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -1,6 +1,7 @@
 # Default imports
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
+from sklearn.model_selection import GridSearchCV
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
@@ -11,6 +12,16 @@ X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
 
-
 # Write your solution here :
-
+def xgboost (X_train, X_test, y_train, y_test,**kwargs):
+    xgb = XGBClassifier()
+    param_grid = {"max_depth": [ 2],
+               "min_child_weight": [ 8],
+               "subsample": [0.6, .7, .8, .9],
+               "colsample_bytree": [0.6, .7, .8, .9, 1]
+               }
+    grid = GridSearchCV(xgb,param_grid,cv = 5)
+    grid.fit(X_train,y_train)
+    y_pred = grid.predict(X_test)
+    accuracy = accuracy_score(y_test,y_pred)
+    return accuracy.item()
