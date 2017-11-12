4aa02501f95fb6ad8139c4224f7c160f5896bdc3
apoorvabanubakode <apoorva.banubakode@gmail.com>
2017-11-12 16:11:38 +0000

    Done

--- q02_param2/build.py
+++ q02_param2/build.py
@@ -21,32 +21,23 @@ param_grid2 = {"gamma": [0, 0.05, 0.1, 0.3, 0.7, 0.9, 1],
 
 
 # Write your solution here :
-def myXGBoost(X_train, X_test, y_train, y_test,model,param_grid,KFold=3):
-    search=GridSearchCV(estimator=model,param_grid=param_grid,cv=KFold)
-    search.fit(X_train,y_train)
 
-    ypred=search.predict(X_test)
-# Write your solution here :
-    ac=accuracy_score(y_test,ypred)
-#    print ac
-    bst_param=search.best_params_
-    model.set_params(**bst_param)
-    #print(type(bst_param))
-    return ac,bst_param
 
-param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
-               "min_child_weight": [4, 6, 7, 8],
-               "subsample": [0.6, .7, .8, .9, 1],
-               "colsample_bytree": [0.6, .7, .8, .9, 1]
-               }
 def param2(X_train, X_test, y_train, y_test,model,param_grid):
 
-    h,j=myXGBoost(X_train, X_test, y_train, y_test,model,param_grid,KFold=3)
-    print model
+    param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
+                   "min_child_weight": [4, 6, 7, 8],
+                   "subsample": [0.6, .7, .8, .9, 1],
+                   "colsample_bytree": [0.6, .7, .8, .9, 1]
+                   }
+    ac,bst=myXGBoost(X_train, X_test, y_train, y_test,model,param_grid1,KFold=3)
+    m1=model.set_params(**bst)
+    h,j=myXGBoost(X_train, X_test, y_train, y_test,m1,param_grid,KFold=3)
+    #print model
     return h,j
 #model=XGBClassifier(seed=9,max_depth=2,min_child_weight= 4,subsample=.8,colsample_bytree=.7)
 #model=XGBClassifier(seed=9)
 
-#ac,bst=myXGBoost(X_train, X_test, y_train, y_test,model,param_grid1,KFold=3)
+
 #h,j=param2(X_train, X_test, y_train, y_test,model,param_grid2)
-#print h,j    
+#print h,j
