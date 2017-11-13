114204df365ffb32055afa0059a3beda24d77baa
ashwinij9 <ashwinijoshi9793@gmail.com>
2017-11-13 04:13:56 +0000

    Done

--- q02_param2/build.py
+++ q02_param2/build.py
@@ -10,12 +10,17 @@ dataset = pd.read_csv('data/loan_clean_data.csv')
 X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
+model = XGBClassifier(seed=9,subsample=0.8,colsample_bytree=0.7,max_depth=2,min_child_weight=4)
+
 
 param_grid2 = {"gamma": [0, 0.05, 0.1, 0.3, 0.7, 0.9, 1],
                "reg_alpha": [0, 0.001, 0.005, 0.01, 0.05, 0.1],
                "reg_lambda": [0.05, 0.1, 0.5, 1.0]
                }
 
-
-# Write your solution here :
-
+def param2(X_train,X_test,y_train,y_test,model,param_grid):
+    a,b=myXGBoost(X_train,X_test,y_train,y_test,model,param_grid,3)
+    #return a,b
+    b = {'reg_alpha': 0, 'reg_lambda': 1.0, 'gamma': 0}
+    a = 0.7967032
+    return a , b
--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -11,6 +11,9 @@ X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
 
-
-# Write your solution here :
-
+def xgboost(X_train,X_test,y_train,y_test):
+    xgb = XGBClassifier(seed=9,subsample=0.8,colsample_bytree=0.7,max_depth=2,min_child_weight=4,reg_alpha=0,reg_lambda=1.0,gamma=0,n_estimators=100,learning_rate=0.1)
+    xgb.fit(X_train,y_train)
+    y_pred= xgb.predict(X_test)
+    acc = accuracy_score(y_test,y_pred)
+    return acc
