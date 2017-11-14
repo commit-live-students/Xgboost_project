2602d144e3129a1b447398f9475698879191c047
sudharsanasai <Sudharsanasai@gmail.com>
2017-11-14 03:17:07 +0000

    Done

--- q02_param2/build.py
+++ q02_param2/build.py
@@ -1,9 +1,11 @@
+# %load q02_param2/build.py
 # Default imports
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 import pandas as pd
 from greyatomlib.Xgboost_project.q01_myXGBoost.build import myXGBoost
-
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score
 # load data
 dataset = pd.read_csv('data/loan_clean_data.csv')
 # split data into X and y
@@ -16,6 +18,29 @@ param_grid2 = {"gamma": [0, 0.05, 0.1, 0.3, 0.7, 0.9, 1],
                "reg_lambda": [0.05, 0.1, 0.5, 1.0]
                }
 
+# param_grid1 = {'subsample': [0.8], 'colsample_bytree': [0.7], 'max_depth': [2], 'min_child_weight': [4]}
+
+# param_grid = dict(param_grid1.items()+param_grid2.items())
+
+
+model = XGBClassifier(seed = 9,subsample = 0.8,colsample_bytree = 0.7,max_depth = 2,min_child_weight = 4)
+
+
+
+
+
+def param2(X_train,X_test,y_train,y_test,model,param_grid):
+
+    model2 = GridSearchCV(model,param_grid)
+    model2.fit(X_train,y_train)
+    accuracy = accuracy_score(y_test,model2.predict(X_test))
+    best_params_temp = model2.best_params_
+
+    your_keys=['reg_alpha','reg_lambda', 'gamma']
+    best_params = { your_key: best_params_temp[your_key] for your_key in your_keys }
+    return accuracy,best_params
+
+param2(X_train,X_test,y_train,y_test,model,param_grid2)
+
 
 # Write your solution here :
-
--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
 # load data
 dataset = pd.read_csv('data/loan_clean_data.csv')
@@ -11,6 +12,21 @@ X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
 
+def xgboost(X_train,X_test,y_train,y_test,**kwargs):
+
+    model = XGBClassifier()
+
+    param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
+               "min_child_weight": [4, 6, 7, 8],
+               "subsample": [0.6, .7, .8, .9, 1],
+               "colsample_bytree": [0.6, .7, .8, .9, 1]
+               }
+
+
+    model2 = GridSearchCV(model,param_grid1,cv=3)
+    model2.fit(X_train,y_train)
+    accuracy = accuracy_score(y_test,model2.predict(X_test))
+
+    return accuracy
 
 # Write your solution here :
-
