28ded4d88167ef31660ec49a99321faace40ccef
ms10031990 <ms10031990@yahoo.com>
2018-03-21 05:35:53 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -12,6 +12,16 @@ y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
 
 
-# Write your solution here :
+def xgboost(X_train, X_test, y_train, y_test, **kwargs):
 
+    xgb = XGBClassifier(subsample=0.8,
+      colsample_bytree=0.7, max_depth=2,
+      min_child_weight=4, reg_alpha=0, reg_lambda=1.0,
+     gamma=0,n_estimators=100,learning_rate=0.01)
+    xgb.fit(X_train, y_train)
+    y_pred = xgb.predict(X_test)
+    y_pred = predictions = [round(value) for value in y_pred]
 
+    acc = accuracy_score(y_test, y_pred)
+
+    return acc
