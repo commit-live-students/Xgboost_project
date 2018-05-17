168c3338f2d6f70cffb8570d270edafd18e27648
kvin15 <vinayakkhamkar@gmail.com>
2018-05-17 17:59:01 +0000

    Done

--- q02_param2/build.py
+++ q02_param2/build.py
@@ -18,3 +18,23 @@ param_grid2 = {"gamma": [0, 0.05, 0.1, 0.3, 0.7, 0.9, 1],
 
 
 # Write your solution here :
+def param2(X_train, X_test, y_train, y_test, model, param_grid):
+    param_grid1 = {"max_depth": [2, 3, 4, 5, 6, 7, 9, 11],
+               "min_child_weight": [4, 6, 7, 8],
+               "subsample": [0.6, .7, .8, .9, 1],
+               "colsample_bytree": [0.6, .7, .8, .9, 1]
+               }
+    acc_score, best_params = myXGBoost(X_train, X_test, y_train, y_test, model=model, param_grid=param_grid1, KFold=3)
+    # Get the best parameters from iteration-1
+    # Append the best parameters and the new parameters to create
+    # new set of parameters
+    param_grid3 = { key_: [val_] for key_, val_ in best_params.items() }
+    param_grid3.update(param_grid)
+    # Use previous function with new set of parameters (iteration-1 best params and new params param-grid)
+    acc_score1, best_params1 = myXGBoost(X_train, X_test, y_train, y_test, model=model, param_grid=param_grid3)
+    # Return only specific_params that were passed as part of param_grid in dictionary
+    specific_params = {}
+    for key_, value_ in best_params1.items():
+        if key_ not in best_params.keys():
+            specific_params[key_] = best_params1[key_]
+    return acc_score1, specific_params
