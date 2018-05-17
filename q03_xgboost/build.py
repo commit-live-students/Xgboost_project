e3d9781925ca40c56ae54b654522641dd2caabc3
kvin15 <vinayakkhamkar@gmail.com>
2018-05-17 18:34:00 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -2,6 +2,7 @@
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
 
 # load data
@@ -13,5 +14,10 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 
 # Write your solution here :
-
-
+# exercise -3
+def xgboost(X_train, X_test, y_train, y_test, **kwargs):
+    kwargs['random_state'] = 9
+    xgb = XGBClassifier(**kwargs)
+    xgb.fit(X_train, y_train)
+    y_pred_test = xgb.predict(X_test)
+    return accuracy_score(y_true=y_test, y_pred=y_pred_test)
