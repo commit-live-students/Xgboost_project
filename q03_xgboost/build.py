e40ca3547b3099095c97ee08e23ea4185b5bd216
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-05-21 18:55:31 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -13,5 +13,16 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 
 
 # Write your solution here :
+def xgboost(X_train,X_test,y_train,y_test,**kwargs):
+    kwargs1 = kwargs
+    kwargs1['n_estimators'] = 50
+    model = XGBClassifier(**kwargs1)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test,y_pred)
+    return acc
 
-
+'''print(xgboost(X_train,X_test,y_train,y_test,subsample=0.8,
+      colsample_bytree=0.7, max_depth=2,
+      min_child_weight=4, reg_alpha=0, reg_lambda=1.0,
+     gamma=0,n_estimators=100,learning_rate=0.1))'''
