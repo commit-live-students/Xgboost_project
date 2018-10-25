0f3e8517c588fdcf7df3e715e12e19aa42733ff4
palash1512 <palash9515@gmail.com>
2018-10-25 10:35:18 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -15,3 +15,11 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 # Write your solution here :
 
 
+def xgboost(X_train,X_test,y_train,y_test,**kwargs):
+    xgb1 = XGBClassifier(seed=9)
+    if kwargs:
+        xgb1.set_params(**kwargs)
+    xgb1.fit(X_train, y_train)
+    y_pred = xgb1.predict(X_test)
+    accuracy = accuracy_score(y_pred, y_test)
+    return accuracy
