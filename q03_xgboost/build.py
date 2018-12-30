aa9c4b6ec9048843ca99dd7d2adbd72ec3480ee6
sannidh <soniknitr@gmail.com>
2018-12-30 07:39:50 +0000

    Done

--- q03_xgboost/build.py
+++ q03_xgboost/build.py
@@ -1,17 +1,30 @@
+# %load q03_xgboost/build.py
 # Default imports
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
-# load data
 dataset = pd.read_csv('data/loan_clean_data.csv')
-# split data into X and y
+
 X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=9)
 
+xgbc = XGBClassifier(seed=9)
 
-# Write your solution here :
+def xgboost(X_train, X_test, y_train, y_test, **kwargs):
+    best_params1 = {'colsample_bytree': 0.7,
+                    'max_depth': 2,
+                    'min_child_weight': 4,
+                    'subsample': 0.8,
+                    'gamma': 0,
+                    'reg_alpha': 0,
+                    'reg_lambda': 1.0}
+    xgbc.set_params(**best_params1)
+    xgbc.fit(X_train, y_train)
+    y_pred = xgbc.predict(X_test)
+    return accuracy_score(y_test,y_pred)
+
 
 
